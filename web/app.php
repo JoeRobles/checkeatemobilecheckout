d3abe57b393ffafa8e2577e01ebb014542dc78da
Joe Robles <joe.robles.pdj@gmail.com>
2017-04-11 13:12:21 -0500

    culqi implemented on backend

--- web/app.php
+++ web/app.php
@@ -4,6 +4,10 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Culqi\Culqi;
+
 $app->get('/', function() use($app) {
     return
   "<!DOCTYPE html>\n" .
@@ -13,6 +17,11 @@ $app->get('/', function() use($app) {
   "    </head>\n" .
   "    <body>\n" .
   "        <p>Hello</p>\n" .
+  "\n" .
+  "        <form id=\"pagar\" method=\"post\" action=\"/verify\" class=\"hidden\">\n" .
+  "            <input id=\"token\" type=\"hidden\" name=\"token\" value=\"\">\n" .
+  "            <input id=\"datos\" type=\"hidden\" name=\"datos\" value=\"\">\n" .
+  "        </form>" .
   "        <button id=\"buyButton\" type=\"button\">Click Me!</button>\n" .
   "        <script src=\"https://checkout.culqi.com/v2\"></script>\n" .
   "        <script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js\"></script>\n" .
@@ -25,6 +34,12 @@ $app->get('/', function() use($app) {
   "                description: 'Polo Culqi lover',\n" .
   "                amount: 3500\n" .
   "            });\n" .
+  "\n" .
+  "            var datos = {\n" .
+  "                nombre: 'Checkeate Store',\n" .
+  "                moneda: 'USD',\n" .
+  "                guardar: false\n" .
+  "            };\n" .
   "\n" .
   "            $('#buyButton').on('click', function(e) {\n" .
   "                console.log('clicked');\n" .
@@ -42,9 +57,81 @@ $app->get('/', function() use($app) {
   "                    console.log('Culqi error', Culqi.error.mensaje);\n" .
   "                }\n" .
   "            };\n" .
+  "\n" .
+  "            function culqi() {\n" .
+  "                if (Culqi.token) {\n" .
+  "                } else {\n" .
+  "                    alert(Culqi.error.mensaje);\n" .
+  "                }\n" .
+  "\n" .
+  "                if (Culqi.error){\n" .
+  "                    alert(Culqi.error.mensaje);\n" .
+  "                } else {\n" .
+  "                    datos.email = Culqi.token.email;\n" .
+  "\n" .
+  "                    fillForm(Culqi, datos);\n" .
+  "\n" .
+  "                    function fillForm(Culqi, datos) {\n" .
+  "                         $('#token').val(Culqi.token.id);\n" .
+  "                         $('#datos').val(JSON.stringify(datos));\n" .
+  "                         setTimeout(function(){\n" .
+  "                             $('#pagar').submit();\n" .
+  "                         }, 500);\n" .
+  "                     }\n" .
+  "                 }\n" .
+  "             }\n" .
   "        </script>\n" .
   "    </body>\n" .
   "</html>";
+});
+
+$app->post('/verify', function(Request $request) use($app) {
+    $culqi = new Culqi(array(
+        'api_key' => 'sk_test_q6u17xfqYdKaE44q'
+    ));
+
+    $token = $request->get('token');
+    $datos = json_decode($request->get('datos'));
+
+    $charge = $culqi->Charges->create(
+        array(
+            "amount" => 1000,
+            "capture" => true,
+            "currency_code" => $datos->moneda,
+            "description" => "Venta de prueba",
+            "email" => $datos->email,
+            "installments" => 0,
+            "source_id" => "$token"
+        )
+    );
+
+//    return new JsonResponse(array('token' => $token, 'datos' => $datos));
+    return new JsonResponse(array('token' => $token, 'datos' => $datos, 'charge' => $charge));
+});
+
+$app->get('/verify', function() use($app) {
+    $culqi = new Culqi(array(
+        'api_key' => 'sk_test_q6u17xfqYdKaE44q'
+    ));
+
+    $token = 'tkn_test_MLqEPMjpZWBZrBIe';
+    $datos = '{"nombre":"Checkeate Store","moneda":"USD","guardar":false,"email":"zero@set.com"}';
+    $datos = json_decode($datos);
+
+    $charge = $culqi->Charges->create(
+        array(
+            "amount" => 1000,
+            "capture" => true,
+            "currency_code" => $datos->moneda,
+            "description" => "Venta de prueba",
+            "email" => $datos->email,
+            "installments" => 0,
+            "source_id" => "$token"
+        )
+    );
+
+//    return new JsonResponse(array('token' => $token, 'datos' => $datos));
+    return new JsonResponse(array('token' => $token, 'datos' => $datos, 'charge' => $charge));
 });
 
 $app['debug'] = true;
